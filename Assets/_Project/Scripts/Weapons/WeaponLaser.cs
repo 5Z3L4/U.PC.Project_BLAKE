1541bc8a359ce79b27525771b27202ee14bfc644
Cis <lilianna.salamon@gmail.com>
2025-01-26 13:42:41 +0100

    Make laser stop at obstacles

--- Assets/_Project/Scripts/Weapons/WeaponLaser.cs
+++ Assets/_Project/Scripts/Weapons/WeaponLaser.cs
@@ -6,22 +6,36 @@ namespace _Project.Scripts.Weapons
     {
         [SerializeField]
         private Transform attackPointTransform;
+        [SerializeField]
+        private LayerMask mask;
 
         private RangedWeapon rangedWeapon;
         private LineRenderer lineRenderer;
-
+        private float laserRange;
         private void Awake()
         {
             rangedWeapon = GetComponentInParent<RangedWeapon>();
             lineRenderer = GetComponent<LineRenderer>();
+            laserRange = rangedWeapon.Range - 2f;
         }
 
         private void Start()
         {
-            var laserLength = new Vector3(0f, 0f, rangedWeapon.Range - 2f);
+            var laserLength = new Vector3(0f, 0f, laserRange);
 
             transform.position = attackPointTransform.position;
             lineRenderer.SetPosition(1, laserLength);
+        }
+
+        private void Update()
+        {
+            if(Physics.Raycast(new Ray(transform.TransformPoint(lineRenderer.GetPosition(0)), transform.TransformDirection(lineRenderer.GetPosition(1)-lineRenderer.GetPosition(0))), out RaycastHit hit, rangedWeapon.Range-2f, mask))
+            {
+                lineRenderer.SetPosition(1, new Vector3(0, 0, hit.distance));
+            } else
+            {
+                lineRenderer.SetPosition(1, new Vector3(0,0, laserRange));
+            }
         }
     }
 }
